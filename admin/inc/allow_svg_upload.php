f37f258a9a202bb85d5aca648dec5ac53163947c
Kristian Pospis <kristian.pospis@gmail.com>
2021-05-18 16:31:59 +0200

    fixed script tag

--- admin/inc/allow_svg_upload.php
+++ admin/inc/allow_svg_upload.php
@@ -8,12 +8,12 @@ add_filter('upload_mimes', 'fm_allow_svg_upload');
 
 function fm_fix_svg() {
     echo <<<EOD
-        <script>
+        <style>
             td.media-icon img[src$=".svg"], img[src$=".svg"].attachment-post-thumbnail { 
                 width: 100% !important; 
                 height: auto !important; 
             }
-        </script>
+        </style>
     EOD;
 }
 add_action('admin_head', 'fm_fix_svg');
